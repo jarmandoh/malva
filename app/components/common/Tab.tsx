2d64604b29a5b803361440226a3a825fc19cfad2
janier hernandez <janier007@gmail.com>
2025-06-01 21:52:39 -0500

    Mejorar la renderización del submenú en el componente de pestañas. Ajustar clases CSS y agregar un ícono SVG para la interacción del usuario.

--- app/components/common/Tab.tsx
+++ app/components/common/Tab.tsx
@@ -111,19 +111,28 @@ const Tabs: React.FC = () => {
   // Renderizamos el componente
   const renderSubmenu = (items: MenuItem[]) => {
     return (
-      <ul className={styles.subMenu}>
+      <ul>
         {items.map((item) => (
-          <li key={item.id} className={styles.subMenuItem}>
-            <a href={item.path}>{item.label}</a>
+          <li key={item.id} className={`${styles.menuItem}`} >
+            <a href={item.path}>
+              {item.label}
+            </a>
             {item.items && (
               <button
-                className={styles.subSubMenuToggle}
-                onClick={() => handleSubSubMenuToggle(item.id)}
+                className={styles.subMenuToggle}
+                onClick={() => {
+                  const subhombreItems = items.filter(el => el.id === item.id)[0]?.items || [];
+                  console.log('hombreItems:', subhombreItems);
+                  renderSubmenu(subhombreItems);
+                }}
               >
-                {activeSubSubMenu === item.id ? '' : '>'}
+                {activeSubMenu === item.id ? '' : 
+                <svg viewBox="0 0 24 31" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M15.77 14.745L8.47 8.075C8.36 7.975 8.19 7.975 8.08 8.075C8.03 8.125 8 8.195 8 8.265C8 8.335 8.03 8.405 8.08 8.455L15.18 14.945L8.22 21.565C8.17 21.615 8.14 21.685 8.14 21.755C8.14 21.825 8.17 21.895 8.22 21.945C8.27 21.995 8.34 22.015 8.41 22.015C8.48 22.015 8.56 21.985 8.61 21.935L15.77 15.125C15.82 15.075 15.85 15.005 15.85 14.935C15.85 14.865 15.82 14.795 15.77 14.745Z"></path>
+                </svg>
+                }
               </button>
             )}
-            {activeSubSubMenu === item.id && item.items && renderSubmenu(item.items)}
           </li>
         ))}
       </ul>
